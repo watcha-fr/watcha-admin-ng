6e354605eac8bafe2aa4e548780418bd2fe00fbf
dlamarcheteamnet <d.lamarche@teamnet.fr>
2025-03-17 15:23:21 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -90,6 +90,7 @@ const App = () => (
     <Resource name="servernotices" />
     <Resource name="forward_extremities" />
     <Resource name="room_state" />
+    <Resource name="dashboard" />
   </Admin>
 );
 
