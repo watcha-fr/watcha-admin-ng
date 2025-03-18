ac2ea962113c2c86a0888b3320def0d92da8ad0f
dlamarcheteamnet <d.lamarche@teamnet.fr>
2025-03-18 15:19:07 +0100

    Update DashboardTab.js

--- src/components/DashboardTab.js
+++ src/components/DashboardTab.js
@@ -3,18 +3,38 @@ import React from "react";
 const DashboardTab = () => {
     const homeserver = localStorage.getItem("base_url");
     const grafana = homeserver.replace("-core","-dashboard")
-    const grafanaUrl = grafana+"/d-solo/000000012/synapse?orgId=1&panelId=198"; // Remplace avec ton URL Grafana
+    const grafanaUrl = grafana+"/d-solo/000000012/synapse?orgId=1&panelId=";
+    const grafanaMemoryPanelId = "198";
+    const grafanaCPUPanelId = "75";
 
     return (
-        <div style={{ height: "100vh", width: "100%" }}>
+        <div style={{ display: "grid", gap: "16px", gridTemplateColumns: "1fr 1fr" }}>
+        {/* Panel 1 */}
+        <Card>
+          <CardContent>
+            <Typography variant="h6">Panel 1 - CPU Usage</Typography>
             <iframe
-                src={grafanaUrl}
-                width="100%"
-                height="100%"
-                frameBorder="0"
-                allowFullScreen
+              src={grafanaUrl+grafanaCPUPanelId}
+              width="100%"
+              height="300"
+              frameBorder="0"
             />
-        </div>
+          </CardContent>
+        </Card>
+    
+        {/* Panel 2 */}
+        <Card>
+          <CardContent>
+            <Typography variant="h6">Panel 2 - Memory Usage</Typography>
+            <iframe
+              src={grafanaUrl+grafanaMemoryPanelId}
+              width="100%"
+              height="300"
+              frameBorder="0"
+            />
+          </CardContent>
+        </Card>
+      </div>
     );
 };
 
